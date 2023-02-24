efb08792e92c4f245502ae3640d95dcb3215060c
Guillaume Lauzier <55703540+guillaumelauzier@users.noreply.github.com>
2023-02-25 00:27:51 +0100

    Create context.js

---
+++ context.js
@@ -0,0 +1,7 @@
+/*
+Create an audio context: The first step is to create an 
+AudioContext object, which provides the entry point into 
+the Web Audio API. This can be done using the following code:
+*/
+
+const audioContext = new AudioContext();
